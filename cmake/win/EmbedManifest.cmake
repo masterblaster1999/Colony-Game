a4323a608a0e3a086e40b1404162b7c9e6470856
masterblaster1999 <ollobrains321@gmail.com>
2025-09-19 19:11:10 +1000

    Update EmbedManifest.cmake

--- cmake/win/EmbedManifest.cmake
+++ cmake/win/EmbedManifest.cmake
@@ -1,60 +1,30 @@
 # cmake/win/EmbedManifest.cmake
-# cg_embed_manifest(<target> <manifest_path>)
-
-if(NOT WIN32)
-  function(cg_embed_manifest)
-  endfunction()
-  return()
-endif()
-
-function(cg_embed_manifest target manifest_path)
-  if(NOT TARGET ${target})
-    message(FATAL_ERROR "cg_embed_manifest: target '${target}' does not exist")
+#
+# cg_embed_manifest(<target> <manifest-abs-or-rel-path>)
+#   - Generates a small .rc that references the manifest with a forward-slash path
+#   - Disables the linker's auto-embed to ensure a single manifest (/MANIFEST:NO)
+#
+function(cg_embed_manifest tgt manifest_path)
+  if(NOT WIN32)
+    return()
+  endif()
+  if(NOT TARGET ${tgt})
+    message(FATAL_ERROR "cg_embed_manifest: target '${tgt}' not found")
+  endif()
+  if(NOT EXISTS "${manifest_path}")
+    message(WARNING "cg_embed_manifest: manifest does not exist: ${manifest_path}")
   endif()
 
-  # Normalize path and decide whether to use the provided manifest or a generated default.
-  set(_use_path "")
-  if(manifest_path AND EXISTS "${manifest_path}")
-    file(TO_CMAKE_PATH "${manifest_path}" _use_path)
-  else()
-    # Generate a minimal, safe default manifest.
-    set(_gen "${CMAKE_CURRENT_BINARY_DIR}/${target}_default.manifest")
-    file(WRITE "${_gen}" "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>
-<assembly xmlns=\"urn:schemas-microsoft-com:asm.v1\" manifestVersion=\"1.0\">
-  <assemblyIdentity version=\"1.0.0.0\" processorArchitecture=\"*\" name=\"${target}\" type=\"win32\"/>
-  <description>${target}</description>
-  <trustInfo xmlns=\"urn:schemas-microsoft-com:asm.v3\">
-    <security>
-      <requestedPrivileges>
-        <requestedExecutionLevel level=\"asInvoker\" uiAccess=\"false\"/>
-      </requestedPrivileges>
-    </security>
-  </trustInfo>
-  <compatibility xmlns=\"urn:schemas-microsoft-com:compatibility.v1\">
-    <application>
-      <!-- Windows 10 / 11 -->
-      <supportedOS Id=\"{8e0f7a12-bfb3-4fe8-b9a5-48fd50a15a9a}\"/>
-    </application>
-  </compatibility>
-  <application xmlns:asmv3=\"urn:schemas-microsoft-com:asm.v3\">
-    <asmv3:windowsSettings>
-      <asmv3:dpiAware>true/pm</asmv3:dpiAware>
-    </asmv3:windowsSettings>
-  </application>
-</assembly>
-")
-    file(TO_CMAKE_PATH "${_gen}" _use_path)
-    message(WARNING "cg_embed_manifest: '${manifest_path}' not found; using default manifest for target '${target}'.")
-  endif()
+  file(TO_CMAKE_PATH "${manifest_path}" _manifest_norm)
+  set(_rc "${CMAKE_CURRENT_BINARY_DIR}/${tgt}_manifest.rc")
 
-  # Write an RC that embeds the manifest using a forward-slash path and numeric RT_MANIFEST (24)
-  set(_rc "${CMAKE_CURRENT_BINARY_DIR}/${target}_manifest.rc")
-  file(WRITE "${_rc}" "1 24 \"${_use_path}\"\n") # 1 RT_MANIFEST "path"
-  target_sources(${target} PRIVATE "${_rc}")
-  source_group("Generated" FILES "${_rc}")
+  # 1 is RT_MANIFEST (resource type 24)
+  file(WRITE "${_rc}" "1 24 \"${_manifest_norm}\"\n")
 
-  # Disable automatic linker embedding to ensure we only have the one above.
+  target_sources(${tgt} PRIVATE "${_rc}")
+
   if(MSVC)
-    target_link_options(${target} PRIVATE /MANIFEST:NO)
+    # Guarantee we don't end up with two manifests
+    target_link_options(${tgt} PRIVATE /MANIFEST:NO)
   endif()
 endfunction()
