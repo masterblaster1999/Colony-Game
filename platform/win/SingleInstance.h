17b03ce289a65e64b48ade68ec3080df3a92faa4
masterblaster1999 <ollobrains321@gmail.com>
2025-09-20 19:42:24 +1000

    Update SingleInstance.h

--- platform/win/SingleInstance.h
+++ platform/win/SingleInstance.h
@@ -1,17 +1,6 @@
+// SingleInstance.h
 #pragma once
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-
-class SingleInstanceGuard {
-    HANDLE m_mutex = nullptr;
-public:
-    explicit SingleInstanceGuard(const wchar_t* name) {
-        m_mutex = CreateMutexW(nullptr, FALSE, name);
-    }
-    bool already_running() const {
-        return (m_mutex && GetLastError() == ERROR_ALREADY_EXISTS);
-    }
-    ~SingleInstanceGuard() {
-        if (m_mutex) CloseHandle(m_mutex);
-    }
-};
+namespace app::single_instance {
+    // returns false if another instance is already running
+    bool acquire();
+}
