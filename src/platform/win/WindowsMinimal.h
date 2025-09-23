148e8e9d7d7855e18d6a837810a50d6a037c16fb
masterblaster1999 <ollobrains321@gmail.com>
2025-09-23 15:04:06 +1000

    Create WindowsMinimal.h

---
+++ src/platform/win/WindowsMinimal.h
@@ -0,0 +1,9 @@
++++ b/src/platform/win/WindowsMinimal.h
++#pragma once
++#ifndef NOMINMAX
++#  define NOMINMAX
++#endif
++#ifndef WIN32_LEAN_AND_MEAN
++#  define WIN32_LEAN_AND_MEAN
++#endif
++#include <Windows.h>
