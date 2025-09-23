441065689f2a7fb20a6743a291bd30ea42648c9f
masterblaster1999 <ollobrains321@gmail.com>
2025-09-23 11:35:31 +1000

    Create RNG.hpp

---
+++ src/worldgen/RNG.hpp
@@ -0,0 +1,8 @@
+// src/worldgen/RNG.hpp
+#pragma once
+#include "RNGCore.hpp"
+#include "RNGBounded.hpp"
+#include "RNGDistributions.hpp"
+#include "RNGGeometry.hpp"
+#include "RNGAlias.hpp"
+#include "RNGUtils.hpp"
