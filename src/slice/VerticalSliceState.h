42bc7391200dd69f0423ba954b903fb316fefc1e
masterblaster1999 <ollobrains321@gmail.com>
2025-09-29 10:20:30 +1000

    Create VerticalSliceState.h

---
+++ src/slice/VerticalSliceState.h
@@ -0,0 +1,8 @@
+// VerticalSliceState.h
+#pragma once
+namespace slice { struct VerticalSlice; }
+extern slice::VerticalSlice g_slice;
+
+// VerticalSlice.cpp (top)
+#include "VerticalSliceState.h"
+slice::VerticalSlice g_slice{};
