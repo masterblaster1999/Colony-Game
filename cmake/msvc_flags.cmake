92181f70c6278ff69972dcb3e32869d48736317d
masterblaster1999 <ollobrains321@gmail.com>
2025-09-29 20:04:05 +1000

    Update msvc_flags.cmake

--- cmake/msvc_flags.cmake
+++ cmake/msvc_flags.cmake
@@ -1,21 +1,10 @@
+# cmake/msvc_flags.cmake
 if(MSVC)
-  # /permissive-, correct __cplusplus, conformance
-  add_compile_options(/permissive- /Zc:__cplusplus /Zc:preprocessor /Zc:inline /Zc:throwingNew /utf-8)
-  add_compile_options(/MP)                   # parallel compilation
-  add_compile_options($<$<CONFIG:Release>:/O2 /Gw /Gy /GL>)
-  add_link_options($<$<CONFIG:Release>:/LTCG>)
-
-  if(MSVC_STATIC_RUNTIME)
-    set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
-  else()
-    set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
-  endif()
-
-  # Warnings
-  add_compile_options(/W4)
+  add_compile_options(/permissive- /Zc:__cplusplus /Zc:preprocessor /Zc:inline /Zc:throwingNew /utf-8 /MP)
+  # Release tuning; keep LTO/IPO under your COLONY_LTO gate from the top-level CMake
+  add_compile_options($<$<CONFIG:Release>:/O2 /Gw /Gy>)
+  add_link_options($<$<CONFIG:Release>:/INCREMENTAL:NO>)
   if(WARNINGS_AS_ERRORS)
     add_compile_options(/WX)
   endif()
 endif()
-set(CMAKE_CXX_STANDARD 20)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
