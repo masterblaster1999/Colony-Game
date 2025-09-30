abbcdc153f25bec3f82a4f8fb10e5cb2af502a33
masterblaster1999 <ollobrains321@gmail.com>
2025-09-30 13:11:56 +1000

    Create test_worldgen_ctx.cpp

---
+++ tests/test_worldgen_ctx.cpp
@@ -0,0 +1,17 @@
+// tests/test_worldgen_ctx.cpp
+#include "worldgen/WorldGen.hpp"
+#include <cassert>
+
+using namespace colony::worldgen;
+
+int main() {
+  GeneratorSettings s{};
+  s.worldSeed = 42;
+  WorldGenerator gen(s);
+  auto a = gen.generate({0,0});
+  auto b = gen.generate({0,0});
+  assert(a.height.width() == b.height.width());
+  assert(a.height.height() == b.height.height());
+  // …consider hashing a few fields to assert equality if operator== isn't defined
+  return 0;
+}
