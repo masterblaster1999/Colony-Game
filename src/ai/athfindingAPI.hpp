e1dc0698644656cbfc27588f2f4e823e29b54423
masterblaster1999 <ollobrains321@gmail.com>
2025-10-01 21:42:07 +1000

    Create athfindingAPI.hpp

---
+++ src/ai/athfindingAPI.hpp
@@ -0,0 +1,10 @@
+// ai/PathfindingAPI.hpp
+#pragma once
+#include "ai/Pathfinding.hpp"   // or the actual defining headers
+
+namespace cg::pf {
+  using ::pf::GridView;
+  using ::pf::Point;
+  using ::pf::Result;
+  using ::pf::aStar;
+}
