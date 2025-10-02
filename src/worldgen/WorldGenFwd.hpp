b57216ff9e588660acf1518b9448b295f32b7936
masterblaster1999 <ollobrains321@gmail.com>
2025-10-02 14:37:02 +1000

    Update WorldGenFwd.hpp

--- src/worldgen/WorldGenFwd.hpp
+++ src/worldgen/WorldGenFwd.hpp
@@ -1,9 +1,7 @@
 #pragma once
-#include <memory>
 namespace colony::worldgen {
-    struct GeneratorSettings;  // actually defined in StageContext.hpp
-    struct StageContext;       // defined in StageContext.hpp
-    struct WorldChunk;         // forward-declare if used by interface
-    struct IWorldGenStage;     // defined where your stage interface lives
-    using StagePtr = std::unique_ptr<IWorldGenStage>;
+  struct GeneratorSettings;
+  struct StageContext;
+  struct WorldChunk;
+  struct ChunkCoord;
 } // namespace colony::worldgen
