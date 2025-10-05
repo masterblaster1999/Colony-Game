824c0f54920a5de68193466d57fce44491f07d93
masterblaster1999 <ollobrains321@gmail.com>
2025-10-06 09:00:28 +1000

    Update quad_ps.hlsl

--- shaders/quad_ps.hlsl
+++ shaders/quad_ps.hlsl
@@ -1,2 +1,23 @@
-struct PSIn { float4 pos : SV_Position; float2 uv : TEXCOORD0; };
-float4 PSMain(PSIn i) : SV_Target { return float4(i.uv, 0.5, 1.0); }
+#include "common/common.hlsli"
+
+// Bindings: same registers you've likely used so far.
+Texture2D    gMainTex        : register(t0);
+SamplerState gLinearSampler  : register(s0);
+
+// Optional output transforms:
+// #define CG_TONEMAP_ACES 1
+// #define CG_OUTPUT_SRGB  1   // if your swap chain expects sRGB-encoded color
+
+float4 PSMain(PSIn pin) : SV_Target
+{
+    float4 col = gMainTex.Sample(gLinearSampler, pin.Tex);
+
+#if defined(CG_TONEMAP_ACES)
+    col.rgb = cg_aces_approx(col.rgb);
+#endif
+
+#if defined(CG_OUTPUT_SRGB)
+    col.rgb = cg_linear_to_srgb(col.rgb);
+#endif
+    return col;
+}
