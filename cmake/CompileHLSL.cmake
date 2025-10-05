884b2d0eb338235b7731137257a4a60827f6d972
masterblaster1999 <ollobrains321@gmail.com>
2025-10-05 18:29:52 +1000

    Update CompileHLSL.cmake

--- cmake/CompileHLSL.cmake
+++ cmake/CompileHLSL.cmake
@@ -1,15 +1,18 @@
-# cmake/CompileHLSL.cmake
-function(compile_hlsl OUTVAR)
-  set(options)
-  set(oneValueArgs TARGET SHADER ENTRY STAGE CONFIG)
-  cmake_parse_arguments(HLSL "${options}" "${oneValueArgs}" "" ${ARGN})
-
-  set(out "${CMAKE_BINARY_DIR}/shaders/${HLSL_SHADER}.${HLSL_STAGE}.bin")
+# Requires: DXC_EXECUTABLE cache var or find_program
+function(compile_hlsl OUTVAR SHADER ENTRY PROFILE)
+  get_filename_component(_name "${SHADER}" NAME_WE)
+  set(_out "${CMAKE_CURRENT_BINARY_DIR}/${_name}.cso")
   add_custom_command(
-    OUTPUT "${out}"
-    COMMAND ${CMAKE_COMMAND} -E make_directory "${CMAKE_BINARY_DIR}/shaders"
-    COMMAND dxc -T ${HLSL_STAGE}_6_6 -E ${HLSL_ENTRY} "$<IF:$<CONFIG:Debug>,-Zi,-O3>" -Fo "${out}" "${HLSL_SHADER}"
-    DEPENDS "${HLSL_SHADER}"
-    VERBATIM)
-  set(${OUTVAR} "${out}" PARENT_SCOPE)
+    OUTPUT "${_out}"
+    COMMAND "${DXC_EXECUTABLE}"
+            -T "${PROFILE}"      # e.g., vs_6_7 / ps_6_7
+            -E "${ENTRY}"        # e.g., VSMain / PSMain
+            -Fo "${_out}"
+            -Qembed_debug -Zi    # embed debug info for RenderDoc/PIX
+            -all_resources_bound
+            "${SHADER}"
+    DEPENDS "${SHADER}"
+    VERBATIM
+  )
+  set(${OUTVAR} "${_out}" PARENT_SCOPE)
 endfunction()
