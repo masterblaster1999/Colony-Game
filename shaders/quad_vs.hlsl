32133c950f1fec7c39bd51bb0ce20be78aab6eeb
masterblaster1999 <ollobrains321@gmail.com>
2025-10-06 09:00:11 +1000

    Update quad_vs.hlsl

--- shaders/quad_vs.hlsl
+++ shaders/quad_vs.hlsl
@@ -1,5 +1,11 @@
-struct VSIn  { float2 pos : POSITION; float2 uv : TEXCOORD0; };
-struct VSOut { float4 pos : SV_Position; float2 uv : TEXCOORD0; };
-VSOut VSMain(VSIn i) {
-    VSOut o; o.pos = float4(i.pos, 0, 1); o.uv = i.uv; return o;
+#include "common/common.hlsli"
+
+// Entry: VSMain (kept to match your CMake setup)
+PSIn VSMain(VSIn vin)
+{
+    PSIn vout;
+    // Screen-space quad: pass through XY, set Z=0, W=1
+    vout.Position = float4(vin.Position.xy, 0.0f, 1.0f);
+    vout.Tex      = vin.Tex;
+    return vout;
 }
