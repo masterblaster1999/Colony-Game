f3db6ae547cc6255c695cb62c10202b1cc35235b
masterblaster1999 <ollobrains321@gmail.com>
2025-10-05 15:50:42 +1000

    Create AppInfo.h

---
+++ platform/win/AppInfo.h
@@ -0,0 +1,5 @@
+#pragma once
+namespace appinfo {
+  inline constexpr wchar_t kAppNamePretty[] = L"Colony Game";  // e.g., UI, Saved Games
+  inline constexpr wchar_t kAppNameSafe[]   = L"ColonyGame";   // e.g., LocalAppData dir
+}
