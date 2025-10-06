198a3c8855dfe332d8012380189b1c3d83cc404a
masterblaster1999 <ollobrains321@gmail.com>
2025-10-07 08:00:44 +1000

    Update EntryWinMain.cpp

--- src/app/EntryWinMain.cpp
+++ src/app/EntryWinMain.cpp
@@ -1,8 +1,12 @@
-#include <windows.h>
+#include <Windows.h>
+#include "CrashHandler.h"
 #include "App.h"
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int)
 {
+    // Install crash dumps immediately so even early-start failures are captured.
+    InstallCrashHandler(L"ColonyGame");
+
     App app;
     return app.Run(hInstance);
 }
