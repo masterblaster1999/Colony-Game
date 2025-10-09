37699613c8fc1e8b02253fd48bfb682ebaf6d249
masterblaster1999 <ollobrains321@gmail.com>
2025-10-10 06:11:10 +1000

    Update Transform.h

--- src/game/components/Transform.h
+++ src/game/components/Transform.h
@@ -3,15 +3,16 @@
 // This is a compatibility shim for the tests that include
 //   #include "game/components/Transform.h"
 // It tries common locations and extensions without forcing test changes.
+// Patched to prefer the real path at: ../../components/Transform.hpp
 
-#if __has_include("components/Transform.hpp")
-  #include "components/Transform.hpp"
-#elif __has_include("components/Transform.h")
-  #include "components/Transform.h"
-#elif __has_include("../../components/Transform.hpp")
+#if __has_include("../../components/Transform.hpp")
   #include "../../components/Transform.hpp"
 #elif __has_include("../../components/Transform.h")
   #include "../../components/Transform.h"
+#elif __has_include("components/Transform.hpp")
+  #include "components/Transform.hpp"
+#elif __has_include("components/Transform.h")
+  #include "components/Transform.h"
 #elif __has_include("../../ecs/components/Transform.hpp")
   #include "../../ecs/components/Transform.hpp"
 #elif __has_include("../../ecs/components/Transform.h")
