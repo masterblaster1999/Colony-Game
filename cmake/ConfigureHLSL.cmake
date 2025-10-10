8f762383e80142fb3e6416d19a1d05134bd2bbf8
masterblaster1999 <ollobrains321@gmail.com>
2025-10-10 18:29:04 +1000

    Update ConfigureHLSL.cmake

--- cmake/ConfigureHLSL.cmake
+++ cmake/ConfigureHLSL.cmake
@@ -1,5 +1,8 @@
+# cmake/ConfigureHLSL.cmake
 # Auto-configure HLSL sources for Visual Studio generators.
-# Heuristics: *_vs.hlsl => Vertex VSMain, *_ps.hlsl => Pixel PSMain, *_cs.hlsl => Compute CSMain
+# Supports BOTH suffix (_vs/_ps/_cs) AND prefix (VS_/PS_/CS_) naming, and
+# entrypoints of either VS/PS/CS or VSMain/PSMain/CSMain.
+
 function(configure_hlsl_target target)
   if(NOT MSVC)
     return()
@@ -11,34 +14,57 @@ function(configure_hlsl_target target)
   endif()
 
   foreach(_s IN LISTS _srcs)
-    get_filename_component(_ext "${_s}" EXT)
+    get_filename_component(_ext  "${_s}" EXT)
+    get_filename_component(_name "${_s}" NAME_WE)
+
+    # Treat includes as header-only
+    if(_ext STREQUAL ".hlsli")
+      set_source_files_properties(${_s} PROPERTIES HEADER_FILE_ONLY ON)
+      continue()
+    endif()
+
     if(NOT _ext STREQUAL ".hlsl")
       continue()
     endif()
 
-    get_filename_component(_name "${_s}" NAME_WE)
+    set(_shader_type "")
+    set(_entry "")
 
+    # ---- Recognize suffix patterns ----
     if(_name MATCHES "_vs$")
-      set_source_files_properties(${_s} PROPERTIES
-        VS_SHADER_TYPE        "Vertex"
-        VS_SHADER_MODEL       "5.0"
-        VS_SHADER_ENTRYPOINT  "VSMain"
-      )
+      set(_shader_type "Vertex")
+      set(_entry "VSMain")
     elseif(_name MATCHES "_ps$")
-      set_source_files_properties(${_s} PROPERTIES
-        VS_SHADER_TYPE        "Pixel"
-        VS_SHADER_MODEL       "5.0"
-        VS_SHADER_ENTRYPOINT  "PSMain"
-      )
+      set(_shader_type "Pixel")
+      set(_entry "PSMain")
     elseif(_name MATCHES "_cs$")
-      set_source_files_properties(${_s} PROPERTIES
-        VS_SHADER_TYPE        "Compute"
-        VS_SHADER_MODEL       "5.0"
-        VS_SHADER_ENTRYPOINT  "CSMain"
-      )
-    else()
-      # Treat as include / utility: don't try to compile it as a shader.
-      set_source_files_properties(${_s} PROPERTIES HEADER_FILE_ONLY ON)
+      set(_shader_type "Compute")
+      set(_entry "CSMain")
+
+    # ---- Recognize prefix patterns ----
+    elseif(_name MATCHES "^VS_")
+      set(_shader_type "Vertex")
+      # Prefer VS if present, else VSMain
+      set(_entry "VS")
+    elseif(_name MATCHES "^PS_")
+      set(_shader_type "Pixel")
+      set(_entry "PS")
+    elseif(_name MATCHES "^CS_")
+      set(_shader_type "Compute")
+      set(_entry "CS")
     endif()
+
+    if(_shader_type STREQUAL "")
+      # Unknown pattern -> don't try to compile
+      set_source_files_properties(${_s} PROPERTIES HEADER_FILE_ONLY ON)
+      continue()
+    endif()
+
+    # Apply Visual Studio HLSL properties
+    set_source_files_properties(${_s} PROPERTIES
+      VS_SHADER_TYPE       "${_shader_type}"
+      VS_SHADER_MODEL      "5.0"      # D3D11 target; swap to 6.x if using DXC + D3D12
+      VS_SHADER_ENTRYPOINT "${_entry}"
+    )
   endforeach()
 endfunction()
