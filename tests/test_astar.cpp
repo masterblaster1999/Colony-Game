f35223da8d08a98ca1a2b4e706324ed49917113a
masterblaster1999 <ollobrains321@gmail.com>
2025-10-14 15:54:38 +1000

    Update test_astar.cpp

--- tests/test_astar.cpp
+++ tests/test_astar.cpp
@@ -1,34 +1,43 @@
-#include <gtest/gtest.h>
+#include <doctest/doctest.h>
 #include "colony/pathfinding/AStar.hpp"
 
 using namespace colony::pf;
 
-TEST(AStar, StraightLine) {
+TEST_CASE("AStar/StraightLine") {
     GridMap m(32, 8);
-    for (int y=0;y<8;++y) for (int x=0;x<32;++x) m.set_walkable(x,y,1);
+    for (int y = 0; y < 8; ++y)
+        for (int x = 0; x < 32; ++x)
+            m.set_walkable(x, y, 1);
+
     AStar astar(m);
-    auto path = astar.find_path({0,4},{31,4});
-    ASSERT_FALSE(path.empty());
-    // 31 cardinal steps expected
-    EXPECT_EQ(path.length(), 32u);
+    auto path = astar.find_path({0,4}, {31,4});
+
+    REQUIRE_FALSE(path.empty());
+    CHECK(path.length() == 32u); // points, including start
 }
 
-TEST(AStar, Blocked) {
+TEST_CASE("AStar/Blocked") {
     GridMap m(8, 8);
-    for (int y=0;y<8;++y) for (int x=0;x<8;++x) m.set_walkable(x,y,1);
-    for (int y=0;y<8;++y) m.set_walkable(4,y,0);
+    for (int y = 0; y < 8; ++y)
+        for (int x = 0; x < 8; ++x)
+            m.set_walkable(x, y, 1);
+    for (int y = 0; y < 8; ++y)
+        m.set_walkable(4, y, 0);
+
     AStar astar(m);
-    auto path = astar.find_path({1,1},{6,1});
-    EXPECT_TRUE(path.empty());
+    auto path = astar.find_path({1,1}, {6,1});
+    CHECK(path.empty());
 }
 
-TEST(AStar, DiagonalNoCornerCutting) {
+TEST_CASE("AStar/DiagonalNoCornerCutting") {
     GridMap m(4, 4);
-    for (int y=0;y<4;++y) for (int x=0;x<4;++x) m.set_walkable(x,y,1);
-    m.set_walkable(1,0,0); // block top neighbor
-    m.set_walkable(0,1,0); // block left neighbor
+    for (int y = 0; y < 4; ++y)
+        for (int x = 0; x < 4; ++x)
+            m.set_walkable(x, y, 1);
+    m.set_walkable(1,0,0);
+    m.set_walkable(0,1,0);
+
     AStar astar(m);
-    // (0,0)->(1,1) should be disallowed (corner-cut)
-    auto path = astar.find_path({0,0},{1,1});
-    EXPECT_TRUE(path.empty());
+    auto path = astar.find_path({0,0}, {1,1});
+    CHECK(path.empty());
 }
