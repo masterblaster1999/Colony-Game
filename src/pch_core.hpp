ad7d0515530d31e8fae3f5ff1eb23d4716735338
masterblaster1999 <ollobrains321@gmail.com>
2025-10-15 17:16:13 +1000

    Create pch_core.hpp

---
+++ src/pch_core.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+// C++ and STL headers common to core
+#include <cstdint>
+#include <cstdlib>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <array>
+#include <optional>
+#include <variant>
+#include <filesystem>
+#include <unordered_map>
+#include <unordered_set>
+#include <memory>
+#include <algorithm>
+#include <limits>
+#include <cmath>
+#include <cassert>
+
+// Add any engine-core wide headers here (NO graphics/UI libs).
