b91855d0f084fd09ac2f0795b075d03b19dc6df4
masterblaster1999 <ollobrains321@gmail.com>
2025-10-23 08:28:02 +1000

    Update CompileHLSL.cmake

--- cmake/CompileHLSL.cmake
+++ cmake/CompileHLSL.cmake
@@ -1,18 +1,22 @@
-# Requires: DXC_EXECUTABLE cache var or find_program
-function(compile_hlsl OUTVAR SHADER ENTRY PROFILE)
-  get_filename_component(_name "${SHADER}" NAME_WE)
-  set(_out "${CMAKE_CURRENT_BINARY_DIR}/${_name}.cso")
-  add_custom_command(
-    OUTPUT "${_out}"
-    COMMAND "${DXC_EXECUTABLE}"
-            -T "${PROFILE}"      # e.g., vs_6_7 / ps_6_7
-            -E "${ENTRY}"        # e.g., VSMain / PSMain
-            -Fo "${_out}"
-            -Qembed_debug -Zi    # embed debug info for RenderDoc/PIX
-            -all_resources_bound
-            "${SHADER}"
-    DEPENDS "${SHADER}"
-    VERBATIM
-  )
-  set(${OUTVAR} "${_out}" PARENT_SCOPE)
+function(compile_hlsl OUT_VAR)
+  set(options)
+  set(oneValueArgs TARGET ENTRY PROFILE)
+  set(multiValueArgs SOURCES INCLUDES DEFINES)
+  cmake_parse_arguments(H "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+  foreach(SRC ${H_SOURCES})
+    get_filename_component(NAME ${SRC} NAME_WE)
+    set(OUT ${CMAKE_CURRENT_BINARY_DIR}/shaders/${NAME}.cso)
+    add_custom_command(
+      OUTPUT ${OUT}
+      COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/shaders
+      COMMAND dxc.exe -nologo -E ${H_ENTRY} -T ${H_PROFILE}
+              $<$<CONFIG:Debug>:-Od -Zi>
+              $<$<CONFIG:Release>:-O3>
+              ${H_DEFINES} ${H_INCLUDES} -Fo ${OUT} ${SRC}
+      DEPENDS ${SRC}
+      VERBATIM)
+    list(APPEND H_OUTS ${OUT})
+  endforeach()
+  set(${OUT_VAR} ${H_OUTS} PARENT_SCOPE)
 endfunction()
