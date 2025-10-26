b8feabfebf20d57a98897f8e0b3fa3eac8e16986
masterblaster1999 <ollobrains321@gmail.com>
2025-10-26 20:24:05 +1000

    Create WinSDKLean.hpp

---
+++ src/platform/win/WinSDKLean.hpp
@@ -0,0 +1,9 @@
+// src/platform/win/WinSDKLean.hpp
+#pragma once
+#ifndef WIN32_LEAN_AND_MEAN
+#  define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#  define NOMINMAX
+#endif
+#include <windows.h>
