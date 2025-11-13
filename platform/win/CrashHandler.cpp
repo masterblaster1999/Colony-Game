4828f0e53576036a31b7dfe29d6f85a01fffafb1
masterblaster1999 <ollobrains321@gmail.com>
2025-11-13 18:19:29 +1000

    Update CrashHandler.cpp

--- platform/win/CrashHandler.cpp
+++ platform/win/CrashHandler.cpp
@@ -4,7 +4,11 @@
     #ifndef NOMINMAX
     #define NOMINMAX
     #endif
+
+    #ifndef WIN32_LEAN_AND_MEAN
     #define WIN32_LEAN_AND_MEAN
+    #endif
+
     #include <Windows.h>
     #include <ShlObj.h>     // SHGetKnownFolderPath
     #include <string>
