53d7ccc66d62b6454d987b86efc03fb48da720c9
masterblaster1999 <ollobrains321@gmail.com>
2025-11-14 20:35:27 +1000

    Update LauncherInstanceWin.cpp

--- platform/win/LauncherInstanceWin.cpp
+++ platform/win/LauncherInstanceWin.cpp
@@ -1,3 +1,6 @@
+// platform/win/LauncherInstanceWin.cpp
+
+#include "platform/win/WinCommon.h"
 #include "platform/win/LauncherInstanceWin.h"
 
 SingleInstanceGuard::SingleInstanceGuard()
@@ -7,12 +10,16 @@ SingleInstanceGuard::SingleInstanceGuard()
 
 SingleInstanceGuard::~SingleInstanceGuard()
 {
-    if (h_)
+    if (h_) {
         CloseHandle(h_);
+        h_ = nullptr;
+    }
 }
 
 bool SingleInstanceGuard::acquire(const wchar_t* name)
 {
+    // Create a named mutex. If it already exists and is owned by another
+    // process, GetLastError() will return ERROR_ALREADY_EXISTS.
     h_ = CreateMutexW(nullptr, FALSE, name);
     return h_ && GetLastError() != ERROR_ALREADY_EXISTS;
 }
