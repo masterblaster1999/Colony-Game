f6df4c9354a9077094080cb7460b85031eba8afc
masterblaster1999 <ollobrains321@gmail.com>
2025-11-17 21:47:08 +1000

    Update Batch2D_ps.hlsl

--- renderer/Shaders/Batch2D_ps.hlsl
+++ renderer/Shaders/Batch2D_ps.hlsl
@@ -1,3 +1,7 @@
-// renderer/Shaders/Batch2D_ps.hlsl
-struct PSIn { float4 svpos : SV_POSITION; float4 col : COLOR; };
-float4 PSMain(PSIn i) : SV_TARGET { return i.col; }
+#include "common/common.hlsli"
+// struct PSIn from common.hlsli
+
+float4 PSMain(PSIn input) : SV_Target
+{
+    // existing batch rendering logic...
+}
