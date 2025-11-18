f0fa26e0ff678698873d16ef06af19ded0128e47
masterblaster1999 <ollobrains321@gmail.com>
2025-11-18 22:36:04 +1000

    Update FullScreenTriangleVS.hlsl

--- renderer/Shaders/Common/FullScreenTriangleVS.hlsl
+++ renderer/Shaders/Common/FullScreenTriangleVS.hlsl
@@ -1,9 +1,32 @@
-struct VSOut { float4 pos : SV_Position; float2 uv : TEXCOORD0; };
+// renderer/Shaders/Common/FullScreenTriangleVS.hlsl
 
-VSOut main(uint id : SV_VertexID) {
-    float2 pos = float2((id == 2) ? 3.0 : -1.0, (id == 1) ? 3.0 : -1.0);
-    VSOut o; 
-    o.pos = float4(pos, 0.0, 1.0);
-    o.uv  = 0.5 * (pos + 1.0);
+struct VSOutput
+{
+    float4 position : SV_Position;
+    float2 uv       : TEXCOORD0;
+};
+
+VSOutput FullScreenTriangleVS(uint vertexId : SV_VertexID)
+{
+    VSOutput o;
+
+    // standard fullscreen triangle pattern
+    if (vertexId == 0) {
+        o.position = float4(-1.0, -1.0, 0.0, 1.0);
+        o.uv       = float2(0.0, 1.0);
+    } else if (vertexId == 1) {
+        o.position = float4(-1.0, 3.0, 0.0, 1.0);
+        o.uv       = float2(0.0, -1.0);
+    } else {
+        o.position = float4(3.0, -1.0, 0.0, 1.0);
+        o.uv       = float2(2.0, 1.0);
+    }
+
     return o;
+}
+
+// Entry point used by CMake/VS (VS_SHADER_ENTRYPOINT "VSMain")
+VSOutput VSMain(uint vertexId : SV_VertexID)
+{
+    return FullScreenTriangleVS(vertexId);
 }
