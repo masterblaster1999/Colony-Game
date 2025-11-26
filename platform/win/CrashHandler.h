b66a134eb1099f1195147bee189d1cd187b1d495
masterblaster1999 <ollobrains321@gmail.com>
2025-11-26 20:13:58 +1000

    Update CrashHandler.h

--- platform/win/CrashHandler.h
+++ platform/win/CrashHandler.h
@@ -1,12 +1,2 @@
 #pragma once
-
-// Installs the crash handler for the running process.
-// On Windows this wires up the CrashDumpWin facility (minidumps in %LOCALAPPDATA%\{app}\Crashes).
-// Call once very early at startup (before creating threads/windows).
-//
-// Default name "ColonyGame" is used for the crash folder and file prefix.
-#ifdef _WIN32
-void InstallCrashHandler(const wchar_t* appName = L"ColonyGame");
-#else
-inline void InstallCrashHandler(const wchar_t* /*appName*/ = L"ColonyGame") {}
-#endif
+void InstallCrashHandler();   // call early in WinMain()
