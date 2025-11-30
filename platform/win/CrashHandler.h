71a674c2b2c930bbfb69dc7819fa7bff9232f7a9
masterblaster1999 <ollobrains321@gmail.com>
2025-11-30 23:29:01 +1000

    Update CrashHandler.h

--- platform/win/CrashHandler.h
+++ platform/win/CrashHandler.h
@@ -1,2 +1,7 @@
 #pragma once
-void InstallCrashHandler();   // call early in WinMain()
+#include <Windows.h>
+
+namespace CrashHandler {
+  // Call once at startup. dumpDir can be L"." to drop next to the exe.
+  void Install(const wchar_t* dumpDir = L".");
+}
