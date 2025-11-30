0065e8abdae0f42f85fa53eac860f7ea91aa4e66
masterblaster1999 <ollobrains321@gmail.com>
2025-11-30 22:52:47 +1000

    Update CrashHandlerWin.cpp

--- src/platform/win/CrashHandlerWin.cpp
+++ src/platform/win/CrashHandlerWin.cpp
@@ -1,40 +1,28 @@
-#include "CrashHandlerWin.h"
 #include <windows.h>
 #include <DbgHelp.h>
 #include <filesystem>
-#include <string>
 #pragma comment(lib, "Dbghelp.lib")
 
-static LONG WINAPI TopLevelFilter(EXCEPTION_POINTERS* pInfo) {
-    // Where to write the dump
-    wchar_t path[MAX_PATH]{};
-    SYSTEMTIME st{}; GetLocalTime(&st);
-    swprintf_s(path, L"%s\\ColonyGame\\crash\\%04u%02u%02u-%02u%02u%02u.dmp",
-               _wgetenv(L"LOCALAPPDATA"),
-               st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
-
-    std::filesystem::create_directories(std::filesystem::path(path).parent_path());
+static LONG WINAPI UnhandledFilter(EXCEPTION_POINTERS* ep) {
+    SYSTEMTIME st; GetLocalTime(&st);
+    wchar_t path[MAX_PATH];
+    swprintf(path, L"%s\\ColonyCrash_%04d%02d%02d_%02d%02d%02d.dmp",
+             _wgetenv(L"TEMP"), st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
 
     HANDLE hFile = CreateFileW(path, GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
     if (hFile != INVALID_HANDLE_VALUE) {
-        MINIDUMP_EXCEPTION_INFORMATION mei{};
-        mei.ClientPointers = FALSE;
-        mei.ExceptionPointers = pInfo;
-        mei.ThreadId = GetCurrentThreadId();
-
-        // Lightweight dump type that still has stacks, unloaded modules etc.
-        MINIDUMP_TYPE type = (MINIDUMP_TYPE)(
-              MiniDumpWithIndirectlyReferencedMemory
-            | MiniDumpWithUnloadedModules
-            | MiniDumpWithThreadInfo);
-
+        MINIDUMP_EXCEPTION_INFORMATION mei{ GetCurrentThreadId(), ep, FALSE };
         MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hFile,
-                          type, &mei, nullptr, nullptr);
+                          MiniDumpWithIndirectlyReferencedMemory | MiniDumpScanMemory,
+                          &mei, nullptr, nullptr);
         CloseHandle(hFile);
     }
-    return EXCEPTION_EXECUTE_HANDLER; // Let the process die gracefully
+    MessageBoxW(nullptr, L"The game crashed.\nA crash dump was saved to your Temp folder.",
+                L"Colony-Game Crash", MB_ICONERROR);
+    return EXCEPTION_EXECUTE_HANDLER;
 }
 
-void crashwin::install_minidump_writer(const wchar_t*) {
-    SetUnhandledExceptionFilter(TopLevelFilter);
+void InstallCrashHandler() {
+    SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX);
+    SetUnhandledExceptionFilter(UnhandledFilter);
 }
