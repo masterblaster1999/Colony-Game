8c83ae19dc7f3823c65ca73e2eb67c5b835e9f66
masterblaster1999 <ollobrains321@gmail.com>
2025-12-04 07:25:05 +1000

    Create ProceduralGraphRun.cpp

---
+++ src/procgen/ProceduralGraphRun.cpp
@@ -0,0 +1,9 @@
+#include "procgen/ProceduralGraph.hpp"  // brings Graph, Options, declarations, etc.
+
+namespace pg {
+
+void run_procedural_graph(Graph& g, const Options& opt) {
+    // >>> move the function body here exactly as it was in the header <<<
+}
+
+} // namespace pg
