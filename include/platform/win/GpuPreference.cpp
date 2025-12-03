2ee40d31519fc33c6609b3f1551c400484670fb5
masterblaster1999 <ollobrains321@gmail.com>
2025-12-03 12:34:00 +1000

    Create GpuPreference.cpp

---
+++ include/platform/win/GpuPreference.cpp
@@ -0,0 +1,4 @@
+extern "C" {
+  __declspec(dllexport) unsigned long NvOptimusEnablement               = 0x00000001;
+  __declspec(dllexport) int          AmdPowerXpressRequestHighPerformance = 1;
+}
