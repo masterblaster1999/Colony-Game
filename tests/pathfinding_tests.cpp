92aa4f9134ec3f55a8b85c548ad8c0943f31f23f
masterblaster1999 <ollobrains321@gmail.com>
2025-12-07 22:32:39 +1000

    Create pathfinding_tests.cpp

---
+++ tests/pathfinding_tests.cpp
@@ -0,0 +1,11 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
+#include <pathfinding/Jps.hpp>
+
+using namespace colony::path;
+
+// ... a tiny test grid implementing IGrid ...
+
+TEST_CASE("start equals goal") { /* ... */ }
+TEST_CASE("blocked goal returns empty") { /* ... */ }
+// etc.
