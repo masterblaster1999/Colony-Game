669e47c2fc1a89b9d9ef801716b01375d8ae0ccd
masterblaster1999 <ollobrains321@gmail.com>
2025-12-09 23:25:11 +1000

    Update test_worldgen_ctx.cpp

--- tests/test_worldgen_ctx.cpp
+++ tests/test_worldgen_ctx.cpp
@@ -1,16 +1,20 @@
 // tests/test_worldgen_ctx.cpp
+#include <doctest/doctest.h>
 #include "worldgen/WorldGen.hpp"
 #include "worldgen/WorldChunk.hpp"
-#include <cassert>
+
 using namespace colony::worldgen;
 
-int main() {
+TEST_CASE("Worldgen: same seed → deterministic chunk") {
     GeneratorSettings s{};
     s.worldSeed = 42;
-    WorldGenerator gen(s);
+
+    WorldGenerator gen{s};
+
     auto a = gen.generate(ChunkCoord{0, 0});
     auto b = gen.generate(ChunkCoord{0, 0});
-    assert(a.height.width() == b.height.width());
-    assert(a.height.height() == b.height.height());
-    return 0;
+
+    CHECK(a.height.width()  == b.height.width());
+    CHECK(a.height.height() == b.height.height());
+    // You can add hashing/equality asserts later if you expose more fields.
 }
