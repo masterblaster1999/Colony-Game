fe9d34448c982ecd0ed63e2f47881538d6fd7247
masterblaster1999 <ollobrains321@gmail.com>
2025-12-10 07:40:29 +1000

    Update CrashHandler.h

--- src/app/CrashHandler.h
+++ src/app/CrashHandler.h
@@ -1,11 +1,13 @@
+// src/app/CrashHandler.h
 #pragma once
 
 #ifdef _WIN32
-    #include "platform/win/CrashHandlerWin.h"
+    // Make this path truly relative to *this* header so it works regardless of include dirs
+    #include "../../platform/win/CrashHandlerWin.h"
 
     inline void InstallCrashHandler(const wchar_t* appName)
     {
-        // You can pass the name through if you want it configurable.
+        // Allow caller to pass a title; fallback is stable.
         wincrash::InitCrashHandler(appName ? appName : L"Colony Game");
     }
 #else
