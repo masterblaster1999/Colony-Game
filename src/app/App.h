7abad74c9285720a5ac2b369bd2f82c775598d79
masterblaster1999 <ollobrains321@gmail.com>
2025-12-10 07:12:00 +1000

    Update App.h

--- src/app/App.h
+++ src/app/App.h
@@ -2,14 +2,14 @@
 #ifndef NOMINMAX
 #define NOMINMAX
 #endif
-#include <windows.h>
-#include <memory>
+#include <Windows.h>
+#include <cstdint>
+
 #include "../render/DeviceD3D11.h"
 #include "../terrain/ThermalErosion.h"
-#include "../core/FixedTimestep.h"
+#include "../core/FixedTimestep.h"   // defines colony::FixedStepper + FixedSettings
 
-class App
-{
+class App {
 public:
     App();
     int Run(HINSTANCE hInstance);
@@ -19,13 +19,16 @@ private:
     bool CreateMainWindow(HINSTANCE, int nCmdShow);
     void OnResize(UINT w, UINT h);
     void Update(double dt);
-    void Render();
+    void Render();                    // keep simple Render(), ignore alpha
 
 private:
-    HWND                m_hwnd = nullptr;
-    render::DeviceD3D11 m_gfx;
+    HWND                   m_hwnd   = nullptr;
+    render::DeviceD3D11    m_gfx;    // D3D11 device/swapchain wrapper
     terrain::ThermalErosion m_erosion;
-    core::FixedTimestep m_step{ 60.0 };
 
-    UINT m_width = 1280, m_height = 720;
+    // New timing object: 60 Hz fixed ticks, up to 5 catch-up ticks, clamp long frames to 0.25s
+    colony::FixedStepper   m_step{ colony::FixedSettings{ 60.0, 5, 0.25 } };
+
+    UINT m_width  = 1280;
+    UINT m_height = 720;
 };
