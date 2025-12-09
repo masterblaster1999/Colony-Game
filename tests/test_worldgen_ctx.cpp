589d2126b2725883a2c90f9db411ebbed783690d
masterblaster1999 <ollobrains321@gmail.com>
2025-12-09 23:24:57 +1000

    Update test_worldgen_ctx.cpp

--- tests/test_worldgen_ctx.cpp
+++ tests/test_worldgen_ctx.cpp
@@ -2,17 +2,15 @@
 #include "worldgen/WorldGen.hpp"
 #include "worldgen/WorldChunk.hpp"
 #include <cassert>
-
 using namespace colony::worldgen;
 
 int main() {
-  GeneratorSettings s{};
-  s.worldSeed = 42;
-  WorldGenerator gen(s);
-  auto a = gen.generate(ChunkCoord{0, 0});
-  auto b = gen.generate(ChunkCoord{0, 0});
-  assert(a.height.width() == b.height.width());
-  assert(a.height.height() == b.height.height());
-  // …consider hashing a few fields to assert equality if operator== isn't defined
-  return 0;
+    GeneratorSettings s{};
+    s.worldSeed = 42;
+    WorldGenerator gen(s);
+    auto a = gen.generate(ChunkCoord{0, 0});
+    auto b = gen.generate(ChunkCoord{0, 0});
+    assert(a.height.width() == b.height.width());
+    assert(a.height.height() == b.height.height());
+    return 0;
 }
