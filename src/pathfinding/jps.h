95983405b66a7b605088913afae5d8e4a1b10c4d
masterblaster1999 <ollobrains321@gmail.com>
2025-12-10 20:18:32 +1000

    Update jps.h

--- src/pathfinding/jps.h
+++ src/pathfinding/jps.h
@@ -1,8 +1,9 @@
 #pragma once
-
-# In the CMake that defines colony_core (or the target that compiles jps.cpp):
+// In the CMake that defines colony_core (or the target that compiles jps.cpp):
+/*
 target_include_directories(colony_core
   PUBLIC  ${CMAKE_SOURCE_DIR}/include          # your public headers
   PRIVATE ${CMAKE_SOURCE_DIR}/pathfinding
           ${CMAKE_SOURCE_DIR}/hpa
 )
+*/
