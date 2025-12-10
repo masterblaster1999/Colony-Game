e2cbf2edfebf1eb2ceeea545bbd637a43074e484
masterblaster1999 <ollobrains321@gmail.com>
2025-12-10 16:49:09 +1000

    Update jps.cpp

--- src/path/jps.cpp
+++ src/path/jps.cpp
@@ -1,5 +1,26 @@
 // src/path/jps.cpp
-#include "../../pathfinding/jps.h" // declares colony::path::IGrid, Cell, JpsOptions, jps_find_path
+//
+// Robust include: try several likely locations for the JPS public header.
+// MSVC will print a one-line note telling you which path was used.
+
+#if __has_include(<pathfinding/jps.h>)
+  #include <pathfinding/jps.h>
+  #pragma message("jps.cpp: including <pathfinding/jps.h>")
+#elif __has_include("pathfinding/jps.h")
+  #include "pathfinding/jps.h"
+  #pragma message("jps.cpp: including \"pathfinding/jps.h\"")
+#elif __has_include("../../pathfinding/jps.h")
+  #include "../../pathfinding/jps.h"
+  #pragma message("jps.cpp: including \"../../pathfinding/jps.h\"")
+#elif __has_include("jps.h")
+  #include "jps.h"
+  #pragma message("jps.cpp: including \"jps.h\"")
+#elif __has_include("../../include/colony/path/jps.h")
+  #include "../../include/colony/path/jps.h"
+  #pragma message("jps.cpp: including \"../../include/colony/path/jps.h\"")
+#else
+  #error "Cannot locate jps.h. Add ${CMAKE_SOURCE_DIR}/pathfinding to your target_include_directories or add a forwarding header at src/path/jps.h."
+#endif
 
 namespace colony::path {
 
