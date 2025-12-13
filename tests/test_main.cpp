0f44e0af8936c1d1ba07286cfab2bf2e36b9c73b
masterblaster1999 <ollobrains321@gmail.com>
2025-12-13 21:17:30 +1000

    Update test_main.cpp

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,3 +1,4 @@
 // tests/test_main.cpp
+// This must be the ONLY translation unit that defines DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN.
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest/doctest.h"
+#include <doctest/doctest.h>
