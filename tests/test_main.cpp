a12d81f2a3251574a9759d129863c37c554ff6ab
masterblaster1999 <ollobrains321@gmail.com>
2025-12-13 11:38:20 +1000

    Update test_main.cpp

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,3 +1,13 @@
 // tests/test_main.cpp
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest/doctest.h>
+
+int main(int argc, char** argv) {
+    doctest::Context ctx;
+    ctx.applyCommandLine(argc, argv);
+
+    // Example: make CI failures faster
+    // ctx.setOption("abort-after", 1);
+
+    return ctx.run();
+}
